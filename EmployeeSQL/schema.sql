98bb32f517fd8bf9ac2b838fb126b0288a68657e
JV V <jvviloria.inc@gmail.com>
2024-05-29 11:22:31 -0700

    Added ERD and complete schema + data analysis

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -9,32 +9,37 @@ drop table titles
 -- Create tables based on CSV file
 create table departments(
 	dept_no varchar(10),
-	dept_name varchar(30)
+	dept_name varchar(30),
+	primary key (dept_no)
 );
 
 create table dept_emp(
-	emp_no INT,
-	dept_no varchar(30)
+	emp_no INTEGER NOT NULL,
+	dept_no varchar(10)
 );
+
 
 create table dept_manager(
 	dept_no varchar(30),
-	emp_no int
+	emp_no int NOT NULL
 );
 
 create table employees(
-	emp_no int,
-	emp_title_id varchar(30),
-	birth_date varchar(30),
-	first_name varchar(30),
-	last_name varchar(30),
+	emp_no int NOT NULL,
+	emp_title_id varchar(30) NOT NULL,
+	birth_date varchar(30) NOT NULL,
+	first_name varchar(30) NOT NULL,
+	last_name varchar(30) NOT NULL,
 	sex varchar(5),
-	hire_date varchar(30)
+	hire_date varchar(30),
+	foreign key (emp_no) references salaries(emp_no)
 );
 
+
 create table salaries(
-	emp_no int,
-	salary int
+	emp_no int NOT NULL,
+	salary int NOT NULL,
+	primary key (emp_no)
 );
 
 create table titles(
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sql-challenge
+
+## Code Sources
+- XPert Learning Assistance was used to double check code in the [Data Analysis](EmployeeSQL/data_analysis.sql) and [Schema](EmployeeSQL/schema.sql).
+- [QuickDatabaseDiagrams](https://app.quickdatabasediagrams.com/) was used to create the [ERD](EmployeeSQL/sql-challenge_ERD.png) for this project.
+
+> [!NOTE]
+> All code was generated referencing topics covered in class (Module 9).
